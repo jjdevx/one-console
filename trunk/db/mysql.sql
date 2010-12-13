2f6c9b7116da3edff5cbc34d4f3bc0f602e1c4e2
anoochit <anoochit@6de050f6-2ba7-0266-3a3c-17e0add50232>
2010-12-13 04:45:13 +0000

    update mysql schema

--- trunk/db/mysql.sql
+++ trunk/db/mysql.sql
@@ -2,6 +2,8 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
+CREATE SCHEMA IF NOT EXISTS `oneconsole` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `oneconsole` ;
 
 -- -----------------------------------------------------
 -- Table `oneconsole`.`user`
@@ -28,19 +30,37 @@ ENGINE = InnoDB;
 -- Table `oneconsole`.`user_privileges`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `oneconsole`.`user_privileges` (
-  `user_user` VARCHAR(50) NOT NULL ,
-  `privileges_id` INT NOT NULL ,
-  PRIMARY KEY (`user_user`, `privileges_id`) ,
-  INDEX `fk_user_has_privileges_privileges1` (`privileges_id` ASC) ,
-  INDEX `fk_user_has_privileges_user` (`user_user` ASC) ,
+  `user` VARCHAR(50) NOT NULL ,
+  `id` INT NOT NULL ,
+  PRIMARY KEY (`user`, `id`) ,
+  INDEX `fk_user_has_privileges_privileges1` (`id` ASC) ,
+  INDEX `fk_user_has_privileges_user` (`user` ASC) ,
   CONSTRAINT `fk_user_has_privileges_user`
-    FOREIGN KEY (`user_user` )
+    FOREIGN KEY (`user` )
     REFERENCES `oneconsole`.`user` (`user` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_has_privileges_privileges1`
-    FOREIGN KEY (`privileges_id` )
+    FOREIGN KEY (`id` )
     REFERENCES `oneconsole`.`privileges` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `oneconsole`.`template`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `oneconsole`.`template` (
+  `id` INT NOT NULL ,
+  `name` VARCHAR(45) NOT NULL ,
+  `template` TEXT NOT NULL ,
+  `user` VARCHAR(50) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_template_user1` (`user` ASC) ,
+  CONSTRAINT `fk_template_user1`
+    FOREIGN KEY (`user` )
+    REFERENCES `oneconsole`.`user` (`user` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
