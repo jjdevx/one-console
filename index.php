51ec4a5be6c03b0dfba85df15b3953ec97cced3f
anoochit <anoochit@6de050f6-2ba7-0266-3a3c-17e0add50232>
2010-12-14 09:20:49 +0000

    update one class

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <?php
+
 	require 'config.php';
 	include "template/".$template."/header.php";	
+
+	$obone=new One();
+	$obone->service_url=$xmlrpc;
+	$obone->session=$oneadmin.":".sha1($onepassword);
+	
+	/** test class **/	
+	//$xml=$obone->UserPool();
+	//$xml=$obone->UserAuthen("oneadmin", "oneadmin");
+	//$xml=$obone->UserDelete(6);
+	//$xml=$obone->UserAllocate("helen", "helen");
+	//$xml=$obone->UserPasswd(7, "xxx");
+	//$xml=$obone->UserAuthen("helen", "xxx");
+	
+	//var_dump($xml);
+
+	include "template/".$template."/footer.php"; 
+
 ?>
-<?php 
-
-$obone=new One();
-$obone->service_url=$xmlrpc;
-$obone->session=$oneadmin.":".sha1($onepassword);
-
-/** test class **/
-
-//$xml=$obone->UserPool();
-//$xml=$obone->UserAuthen("oneadmin", "oneadmin");
-//$xml=$obone->UserDelete(6);
-//$xml=$obone->UserAllocate("helen", "helen");
-//$xml=$obone->UserPasswd(7, "xxx");
-//$xml=$obone->UserAuthen("helen", "xxx");
-
-//var_dump($xml);
-
-
-
-
-?> 
-<?php include "template/".$template."/footer.php"; ?>
