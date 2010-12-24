dfd7a53783ec65e4ec2dd7979e00b53d48875d55
anoochit <anoochit@6de050f6-2ba7-0266-3a3c-17e0add50232>
2010-12-24 08:04:38 +0000

    add lostpassword module

---
+++ module/lostpassword/lostpassword.php
@@ -0,0 +1,5 @@
+<form id="lostpassword" name="lostpassword" method="post" >
+<label for="username">Username</label>
+<input type="text" name="username">
+<input type="submit" value="Reset Password">
+</form>
